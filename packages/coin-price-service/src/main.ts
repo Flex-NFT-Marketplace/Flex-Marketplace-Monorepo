bdf9362e289fddae0323ab412e7d57e77cdf0613
BrianSin <briansinw3b@users.noreply.github.com>
2025-05-21 17:27:01 +0700

    config coin price port

--- packages/coin-price-service/src/main.ts
+++ packages/coin-price-service/src/main.ts
@@ -1,8 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import configuration from '@app/shared/configuration';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  await app.listen(configuration().coin_price_port, () => {
+    console.log(
+      `coin price service is running on port ${configuration().coin_price_port}`,
+    );
+  });
 }
 bootstrap();
+// AppClusterService.clusterize(bootstrap)
